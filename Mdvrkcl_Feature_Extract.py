2c5ff228033236c7ba0d70b4972c48e8bcb5b483
Amran Hossain <amran.h063@gmail.com>
2026-04-09 11:42:16 +0200

    Extract MFFC and Lower level base features
    Split the wav files into chunks and extract audio features

---
+++ Mdvrkcl_Feature_Extract.py
@@ -0,0 +1,123 @@
+
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import glob
+import os.path
+from feature_extraction_MDVRKCL import Feature_Extraction
+import pandas as pd
+
+
+f = Feature_Extraction()
+
+
+# Split the wav files into chunks
+
+
+
+def split_into_chunks():
+    folder_paths = [r"Dataset_MDVR_KCL/ReadText/HC/*.wav", r"Dataset_MDVR_KCL/ReadText/PD/*.wav"]
+    parent_dirs = [r"Dataset_MDVR_KCL/MDVR/ReadText/chunks/HC", r"Dataset_MDVR_KCL/MDVR/ReadText/chunks/PD"]
+
+    for i in range(len(folder_paths)):
+        for file in glob.glob(folder_paths[i]):
+            try:
+                print(file)
+                # split to get HC\ID00
+                path2, filename2 = os.path.split(file)
+                root, ext = os.path.splitext(filename2)
+                x = root.split('_')[0]
+
+                directory = x
+                parent_dir = parent_dirs[i]  # r"dataset\MDVR\HC"
+
+                path = os.path.join(parent_dir, directory)
+                print(path)
+                os.makedirs(path)
+
+                filename = file
+                sound_file = AudioSegment.from_wav(filename)
+                audio_chunks = split_on_silence(sound_file,
+                                                # must be silent for at least half a second
+                                                min_silence_len=1000,
+                                                # consider it silent if quieter than -16 dBFS
+                                                silence_thresh=-40)
+                for j, chunk in enumerate(audio_chunks):
+                    out_file = path + "/chunk{0}.wav".format(j)
+                    print("exporting", out_file)
+                    chunk.export(out_file, format="wav")
+            except Exception as e:
+                print(e)
+                print("error while handling file: ", file)
+
+
+def extract_features_from_chunks(folder_path, label):
+    # folder_path = r"dataset\MDVR\HC"
+    df_all = []
+    for root, dirs, files in os.walk(folder_path):
+        for dir in dirs:
+            folder_path = os.path.join(root, dir)
+            folder_path = r'%s' % folder_path + "/*.wav"
+            print(folder_path)
+            df_hc = f.extract_features_from_folder(folder_path)
+            # print(df_hc)
+            df_all.append(df_hc)
+            # print(df_all_hc)
+    # call the function to extract the features from the folder
+    df_all = pd.concat(df_all)
+    df_all['label'] = label
+    return df_all
+
+
+def extract_mfccfeatures_from_chunks(folder_path, label):
+    # folder_path = r"dataset\MDVR\HC"
+    df_all = []
+    for root, dirs, files in os.walk(folder_path):
+        for dir in dirs:
+            folder_path = os.path.join(root, dir)
+            folder_path = r'%s' % folder_path + "/*.wav"
+            print(folder_path)
+            df_hc = f.extract_mfcc_from_folder(folder_path)
+            # print(df_hc)
+            df_all.append(df_hc)
+            # print(df_all_hc)
+    # call the function to extract the features from the folder
+    df_all = pd.concat(df_all)
+    df_all['label'] = label
+    return df_all
+
+
+
+# extract the features
+def acoustic_features():
+    hc = extract_features_from_chunks(r"Dataset_MDVR_KCL/MDVR/ReadText/chunks/HC", 0)
+    pd_1 = extract_features_from_chunks(r"Dataset_MDVR_KCL/MDVR/ReadText/chunks/PD", 1)
+    df_acoustic_features = pd.concat([hc, pd_1])
+    f.convert_to_csv(df_acoustic_features, "Dataset_MDVR_KCL/MDVR/ReadText/chunks/MDVR_ReadText_acoustic_features_chunks")
+    return df_acoustic_features
+
+
+def mfcc_features():
+    print("Start")
+    hc = extract_mfccfeatures_from_chunks(r"Dataset_MDVR_KCL/MDVR/ReadText/chunks/HC", 0)
+    pd_1 = extract_mfccfeatures_from_chunks(r"Dataset_MDVR_KCL/MDVR/ReadText/chunks/PD", 1)
+    df_mfcc_features = pd.concat([hc, pd_1])
+    f.convert_to_csv(df_mfcc_features, "Dataset_MDVR_KCL/MDVR/ReadText/chunks/MDVR_ReadText_mfcc_features_chunks")
+    print("End")
+    return df_mfcc_features
+
+
+
+
+
+# run the splitting code
+split_into_chunks()
+
+
+
+#df_mel_spectrogram = mel_spectrogram_features()
+df_all_features = pd.merge(acoustic_features(), mfcc_features(), how="left", on="voiceID")
+#df_all_features = acoustic_features().merge(mfcc_features(), on="voiceID", how="left")\
+#                                     .merge(gtcc_features(), on="voiceID", how="left")
+
+f.convert_to_csv(df_all_features, "Dataset_MDVR_KCL/MDVR/ReadText/chunks/MDVR_ReadText_all_features_chunks")
+print(df_all_features)
